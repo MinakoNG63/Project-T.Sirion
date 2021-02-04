5f8087fba23be5d02070e397dd773e45141d4bbf
MinakoNG63 <73382317+MinakoNG63@users.noreply.github.com>
2021-02-04 13:00:04 +0700

    Delete lab3.m

--- lab3.m
+++
@@ -1,8 +0,0 @@
-red = imread('red.jpg');
-green = imread('green.jpg');
-blue = imread('blue.jpg');
-
-[XR, YR] = size(red)
-I2 = imcrop(green,[1 1 1196-1 1068-1]);
-figure,imshow(green);
-figure,imshow(I2);
